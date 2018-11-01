9c1fea836ccc6c8ffd14dac7d00c3ece328a3943
rupertlion <rupertlion@gmail.com>
2018-11-01 15:34:01 +0100

    Extracting styling from devise registration to tailwind.scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_141122) do
+ActiveRecord::Schema.define(version: 2018_11_01_101117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
