850aebac3369c24b70c19b5682a33e59c14198fc
Angelica Costantini <angelicacostantini07@gmail.com>
2018-11-01 16:14:37 +0100

    refactoring new_object_step and route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'hospitals/index'
     scope '(:locale)', locale: /en|sv/ do
     root controller: :hospitals, action: :index
   end
--- features/step_definitions/new_object_steps.rb
+++ features/step_definitions/new_object_steps.rb
@@ -1,5 +1,5 @@
 Given("the following hospitals exists") do |table|
     table.hashes.each do |hospital|
-        Hospital.create(hospital)
+        create(:hospital, hospital)
     end
 end
