552810c5789a7647709d12b2dc68291f1414e4f4
rhon <rhoneil.gabriel@me.com>
2018-11-01 20:50:32 +0100

    Added steps for creating companies using factories

--- features/step_definitions/new_object_steps.rb
+++ features/step_definitions/new_object_steps.rb
@@ -1,4 +1,5 @@
 Given("the following staffing companies exists") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
+    table.hashes.each do |staffco|
+        create(:staffco, staffco)
+    end
 end
