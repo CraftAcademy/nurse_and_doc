e25834ed14643d87c9a49f79d1914b0ec0a79222
Zak <zakary.howard94@gmail.com>
2018-11-01 16:19:28 +0100

    refactored html and basic step

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,4 +1,4 @@
-Then("I should see {string} within the {string} section") do |content, sidebar|
+Then("I should see {string} within the {string} section") do |content, section|
     within(:css, '#sidebar') {
       expect(page).to have_content content 
     }  
