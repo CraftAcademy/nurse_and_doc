309abb3de7b50667cae2925c566e393aa5a07027
rhon <rhoneil.gabriel@me.com>
2018-11-01 10:33:20 +0100

    modified create action to redirect message to root_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params.merge(password: Devise.friendly_token[0, 20]))
-    @message = if user.persisted?
-                 "RSC user #{User.last.email} created"
-               else
-                 'This did not work'
-               end
+    if user.persisted?
+    redirect_to root_path, notice: "RSC user #{User.last.email} created"
+    else
+      'This did not work'
+    end
   end
 
   private
