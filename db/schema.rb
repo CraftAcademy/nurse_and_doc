d5dbd4d30f1e993df9eba17eb33ef8457f739068
Noel Ryan <noelgerard.ryan@gmail.com>
2018-11-02 11:50:04 +0100

    Pulled from upstream and did a migrate updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_101117) do
+ActiveRecord::Schema.define(version: 2018_11_02_093724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "profession"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
