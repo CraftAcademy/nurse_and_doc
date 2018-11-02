5fac65fee0975a1b67013f1dd8571b3b712dacf1
rupertlion <rupertlion@gmail.com>
2018-11-02 11:35:40 +0100

    Fixing and refactoring step definitions

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,17 +1,5 @@
-Given("I am on the dashboard page") do
-    visit root_path
-end
-
 Then("I should see {string}") do |content|
     expect(page).to have_content content
-end
-
-Then("I click the {string} link") do |link|
-    click_link link
-end
-
-When("I click on the link {string}") do |button|
-    click_on button
 end
 
 Then("I should see {string} within the {string} section") do |content, section|
@@ -26,16 +14,7 @@ end
     }
 end
 
-Then("I am on the hospital page") do
-    visit hospitals_path
-end
-
 Then("I should be on the hospital index") do
     expect(page).to have_current_path(hospitals_path)
 end
 
-When("I click on the link {string} section") do |link, section|
-    within("#{section}") {
-        click_link link
-    }
-end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,9 +2,16 @@ Given("I click on {string}") do |link|
   click_link link 
 end
 
-Given("I click on {string} within the {string} section") do |content, section|
+And("I click on {string} within the {string} section") do |link, section|
   within("#{section}") {
-    expect(page).to have_content content
+    click_link link
   }
 end
 
+Given("I am on the dashboard page") do
+  visit root_path
+end
+
+Then("I am on the hospital page") do
+  visit hospitals_path
+end
