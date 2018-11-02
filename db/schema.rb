eb5820711a90c28eae44807fde5c71839ebf1fcd
Noel Ryan <noelgerard.ryan@gmail.com>
2018-11-02 13:01:46 +0100

    Ran a migration and updates schema and upstream pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_194108) do
+ActiveRecord::Schema.define(version: 2018_11_02_093724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2018_11_01_194108) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "profession"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "staffcos", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
