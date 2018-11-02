6b22864b091a01e3bf50eb71203a9ed84fd88184
Angelica Costantini <angelicacostantini07@gmail.com>
2018-11-02 15:55:39 +0100

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params.merge(password: Devise.friendly_token[0, 20]))
     if user.persisted?
-    redirect_to root_path, notice: "RSC user #{User.last.email} created, currently logged in as #{current_user.email}."
+      redirect_to root_path, notice: "RSC user #{User.last.email} created, currently logged in as #{current_user.email}."
     else
-    redirect_to root_path, notice: "Invalid Email"
+      redirect_to root_path, notice: "Invalid Email"
     end
   end
 
