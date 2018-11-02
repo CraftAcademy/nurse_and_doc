490a981ec54f69f2c0b2a6530bd430e9f3b2dfef
rhon <rhoneil.gabriel@me.com>
2018-11-02 11:36:51 +0100

    fixed resources command in routes for staffcos, test green

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root controller: :jobs, action: :index 
-  resources controller: :staffcos, only: :index
+  resources :staffcos, only: :index
   resources :hospitals, only: :index
   scope '(:locale)', locale: /en|sv/ do
   end
