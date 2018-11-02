73d53b5463a21716450a9cb54f0b40a3732c900e
rhon <rhoneil.gabriel@me.com>
2018-11-02 10:46:29 +0100

    change routes for staffcos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'staffcos/index'
   root controller: :jobs, action: :index 
-  root controller: :staffcos, action: :index 
+  resources controller: :staffcos, only: :index
   resources :hospitals, only: :index
   scope '(:locale)', locale: /en|sv/ do
   end
