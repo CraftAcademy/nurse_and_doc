12567e0a7692759262621ee7127954a8bd7a99f7
Angelica Costantini <angelicacostantini07@gmail.com>
2018-11-02 16:04:19 +0100

    adding tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-end
+    describe 'DB table' do
+        it { is_expected.to have_db_column :email }
+      end
+    
+      describe 'Validations' do
+        it { is_expected.to validate_presence_of :email }
+      end
+    
+      describe 'Factory' do
+        it 'should have valid Factory' do
+          expect(create(:user)).to be_valid
+        end
+    end
+end 
