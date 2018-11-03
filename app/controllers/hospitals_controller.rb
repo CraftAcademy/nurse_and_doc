b361ca3b030c2ee82df052c1207776f30ba429bf
Angelica Costantini <angelicacostantini07@gmail.com>
2018-11-03 11:41:17 +0100

    adding create and show actions for hospitals

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,4 +2,16 @@ class HospitalsController < ApplicationController
   def index
     @hospitals = Hospital.all
   end
+
+  def show
+    @hospital = Hospital.find(params[:name])
+  end
+
+  def create
+    @hospital = Hospital.new(params[:hospital])
+ 
+    @hospital.save
+    redirect_to @hospital
+  end 
+
 end
