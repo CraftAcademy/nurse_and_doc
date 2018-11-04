b7ffca6fca79de81992b1c53de44ed2118fbbf3d
Noel Ryan <noelgerard.ryan@gmail.com>
2018-11-04 12:09:23 +0100

    Refactored the from and have it working currently not saving but can click on the hospital page and see the form

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -3,24 +3,17 @@ class HospitalsController < ApplicationController
     @hospitals = Hospital.all
   end
   
-  def new
-  end
-
-  def show
-    @hospital = Hospital.find(params[:name])
-  end
-
   def create
-    hospital = Hospital.create(hospital_params)
-    binding.pry
-    if @hospital.save
+    hospital = Hospital.new(hospital_params)
+    if @hospitals.save
       redirect_to hospitals_path
     else
-      redirect_to hospitals_path, notice: "Hospital already exists. Your hospital could not be saved."
+      render'new'
     end
   end
 
   private
+  
   def hospital_params
     params.require(:hospital).permit(:name)
   end
