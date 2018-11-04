64f6262ad009d2e21ce308cfb2e73c2f039144f2
Zak <zakary.howard94@gmail.com>
2018-11-04 10:43:57 +0100

    added new, create and job params for job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,22 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def job_params
+    params.require(:job).permit(:name)
+  end
+
 end
