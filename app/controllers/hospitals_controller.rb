1f6bf4c763ae58be21cebc43e6d6664bd0bfdada
rhon <rhoneil.gabriel@me.com>
2018-11-05 14:04:39 +0100

    added uniqueness of hospital namel, changed hospital view

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -7,12 +7,15 @@ class HospitalsController < ApplicationController
   end
   
   def create
-    @hospital = Hospital.new(hospital_params)
-    if @hospital.save
+    unless Hospital.exists?(hospital_params)
+    
+    hospital = Hospital.create(hospital_params)
+    if hospital.persisted?
       redirect_to hospitals_path
     else
-      errors = user.errors.full_messages
+      errors = hospital.errors.full_messages
       render json: { message: errors }, status: 422
+      end
     end
   end
 
