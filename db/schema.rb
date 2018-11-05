62266b028d6262fe3768a931875c570f98ca9958
rhon <rhoneil.gabriel@me.com>
2018-11-05 08:51:42 +0100

    added phone number column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_074720) do
+ActiveRecord::Schema.define(version: 2018_11_05_075111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_074720) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "personal_name"
+    t.string "phone"
   end
 
   create_table "users", force: :cascade do |t|
