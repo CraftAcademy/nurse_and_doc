9ff1951dd42e0a560902dccd3571bd8f674353b2
Zak <zakary.howard94@gmail.com>
2018-11-05 14:07:51 +0100

    checking for job columns in spec file

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,7 +2,13 @@ RSpec.describe Job, type: :model do
   describe 'DB table' do
   it { is_expected.to have_db_column :name }
   it { is_expected.to have_db_column :profession }
-  it { is_expected.to have_db_column :scope }
+  it { is_expected.to have_db_column :hospital }
+  it { is_expected.to have_db_column :department }
+  it { is_expected.to have_db_column :license }
+  it { is_expected.to have_db_column :caretype }
+  it { is_expected.to have_db_column :working_hours }
+  it { is_expected.to have_db_column :date_start }
+  it { is_expected.to have_db_column :date_finish }
 end
 
   describe 'Validations' do
