9ba0701704ea217de3c68aabecaac4c855cbe41d
rhon <rhoneil.gabriel@me.com>
2018-11-06 16:42:05 +0100

    run db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_105919) do
+ActiveRecord::Schema.define(version: 2018_11_06_083824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,20 @@ ActiveRecord::Schema.define(version: 2018_11_05_105919) do
     t.string "date_finish"
   end
 
+  create_table "licenses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "staffcos", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "contact_name"
+    t.string "phone"
+    t.string "webhook"
   end
 
   create_table "users", force: :cascade do |t|
