9bf8c7db42252d69dbe493eb8b7e34538b55bab6
Zak <zakary.howard94@gmail.com>
2018-11-06 11:18:27 +0100

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,21 @@ Job.destroy_all
 Staffco.destroy_all
 License.destroy_all
 
-Hospital.create!(name: "Karolinska")
-Hospital.create!(name: "Södersjukhuset")
-Hospital.create!(name: "Martina Children's Hospital")
-Hospital.create!(name: "Stockholms Sjukhem")
-Hospital.create!(name: "GHP Arytmi Center Stockholm")
+Hospital.create!([
+    { name: "Karolinska" }, { name: 'Södersjukhuset' }, { name: "Martina Childrens Hospital" },
+    { name: "Stockholms Sjukhem" }, { name: "GHP Arytmi Center Stockholm" }
+])
 
-Job.create!(name: "Doctor")
-Job.create!(name: "Nurse")
-Job.create!(name: "Assistant Nurse")
+Job.create!([
+    { name: "Doctor" }, { name: "Nurse" }, { name: "Assistant Nurse" }
+])
 
-Staffco.create!(name: "Doctors r us")
-Staffco.create!(name: "Nurses 4 U")
-Staffco.create!(name: "Psychologists agency")
-Staffco.create!(name: "Eyedoctors United")
+Staffco.create!([
+    { name: "Doctors r us" }, { name: "Nurses 4 U" }, { name: "Psychologists agency" }, 
+    { name: "Eyedoctors United" }
+])
 
-License.create!([{ name: 'Allmän internmedicin' }, { name: 'Anestesiologi'}, { name: 'Urologi'}, { name: 'Kirurgi'}, { name: 'Allmänmedicin'}, { name: 'Ortopedi'}])
+License.create!([
+    { name: "Allmän internmedicin" }, { name: "Anestesiologi" }, { name: "Urologi" }, 
+    { name: "Kirurgi" }, { name: "Allmänmedicin" }, { name: "Ortopedi" }
+])
