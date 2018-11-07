f18fa492b9c2e3b8d9a79631e4d5b745bcf0341d
Noel Ryan <noelgerard.ryan@gmail.com>
2018-11-07 22:52:18 +0100

    Created a model for regions and added validates presence of name into model

---
+++ app/models/region.rb
@@ -0,0 +1,3 @@
+class Region < ApplicationRecord
+    validates_presence_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_102619) do
+ActiveRecord::Schema.define(version: 2018_11_07_214942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2018_11_07_102619) do
   end
 
   create_table "licenses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
