61bddc7fa2a5dfe4fea36041e02ddeb47414ac7b
rhon <rhoneil.gabriel@me.com>
2018-11-08 08:53:11 +0100

    included invitations to routes, add parameters to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
     # before_action :authenticate_user!
-    before_action :set_locale
+    before_action :set_locale, :configure_permitted_parameters, if: :devise_controller?
 
     private
     def set_locale
         I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :phone])
     end
 end
