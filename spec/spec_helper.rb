7cec15de688d144a4981e05d36432d7c41948654
Zak <zakary.howard94@gmail.com>
2018-11-09 11:27:45 +0100

    setting up spec environment for mail_spec gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
+require "action_mailer"
+require "email_spec"
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
+  end
+
+  config.include EmailSpec::Helpers
+  config.include EmailSpec::Matchers
+
+  config.before(:each) do
+    reset_mailer
   end
 end
