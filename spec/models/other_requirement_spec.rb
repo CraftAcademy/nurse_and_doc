3e0a615ecd3060592babdca016c65c313f04597d
Zak <zakary.howard94@gmail.com>
2018-11-10 21:42:53 +0100

    spec testing for other_requirement includes validation of db column name, validate presence of name and presence of a factory bot

--- spec/models/other_requirement_spec.rb
+++ spec/models/other_requirement_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe OtherRequirement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:other_requirement)).to be_valid
+    end
+  end
 end
