3d840e6b0762d93c1c0b3609019c221f68a13e29
Zak <zakary.howard94@gmail.com>
2018-11-11 13:05:13 +0100

    added new input fields to assertion step and created factory bot step for other requirements

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -52,9 +52,24 @@ Then("I fill in the job form with {string} information") do |content|
     select 'Kirurgi', from: 'License'
     select 'Homecare', from: 'Care type'
     select 'Part Time', from: 'Scope'
+    fill_in 'Requirements', with: 'Experienced, dedicated and leadership skills.'
+    select 'Drivers License', from: 'Other requirements'
+    fill_in 'Description', with: 'Looking for Doctor with a lot of experience.'
+    find(:css, "#job_accomodation_arrangement[value='1']").set(true)
+    fill_in 'Accomodation budget', with: '1000kr per week'
+    fill_in 'Accomodation comment', with: 'If you are in a hotel'
+    find(:css, "#job_travel_arranged[value='1']").set(true)
+    fill_in 'Travel budget', with: '500kr a week'
+    fill_in 'Travel comment', with: 'If you are using a car'
+    find(:css, "#job_other_pay[value='1']").set(true)
+    fill_in 'Other budget', with: '100 - 1000kr'
+    fill_in 'Other comment', with: 'We will subsidize parking fees'
+    fill_in 'Reference number', with: '2000-10-20-3040'
+    select '8', from: 'Years experience'
     select 'Weekend', from: 'Working hours'
     fill_in 'Date start', with: '0202020'
     fill_in 'Date finish', with: '1202021'
+    fill_in 'Application deadline', with: '01012019'
 end
 
 When("I fill in company form with {string} information") do |email|
