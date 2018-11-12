4eafe2c448c55379675100c91befa07deeefa90f
Zak <zakary.howard94@gmail.com>
2018-11-12 14:50:47 +0100

    config action mailer perform delivery. Was not being used for anything

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,7 @@ Rails.application.configure do
 
   # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   config.action_mailer.perform_caching = false
 
