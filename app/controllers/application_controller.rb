1a577a52815b47804dd442e7f7530e96a189a91b
Zak <zakary.howard94@gmail.com>
2018-11-15 13:00:14 +0100

    sign up button no longer searches for region to merge credentials, after sign up function in application controller now takes over

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
 
     def after_sign_in_path_for(resource_or_scope)
+        # binding.pry
         resource = resource_or_scope
         if !resource.region
           flash[:notice] = 'Please set up your region'
-          redirect_to new_region_path
+          redirect_to new_region_path and return
         else
           super
         end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,10 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    region = Region.find(sign_up_params[:region])
-    build_resource(sign_up_params.except(sign_up_params[:region]).merge(region: region))
+    # region = Region.find(sign_up_params[:region])
+    # build_resource(sign_up_params.except(sign_up_params[:region]).merge(region: region))
+    build_resource(sign_up_params.except(sign_up_params[:region]))
     if resource.save
-      flash[:notice] = "RSC user #{resource.email} created within #{resource.region.name} region."
+      # flash[:notice] = "RSC user #{resource.email} created within #{resource.region.name} region."
+      flash[:notice] = "RSC user #{resource.email} created."
       sign_up(resource_name, resource)
       redirect_to after_sign_in_path_for(resource)
     else
