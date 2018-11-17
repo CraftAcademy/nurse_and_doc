1ca9f4fc9e935d477646b8af275448b4cdabde9f
Thomas <thomas@craftacademy.se>
2018-11-17 07:12:51 +0100

    WIP create department interface

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,4 +1,14 @@
 class DepartmentsController < ApplicationController
   def create
+    binding.pry
+    if Department.create(department_params)
+      flash.now[:notice] = "Department has been added"
+    end
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name, :hospital_id)
   end
 end
